bca25c14e9f39b92413c58e93765d0015353a6d7
golfvert <remy.giraud@meteo.fr>
2024-06-06 05:45:01 +0200

    Upgrade to 2.1.0 (schema validation)

--- docker-wis2gb.sh
+++ docker-wis2gb.sh
@@ -13,4 +13,4 @@ docker build --rm --no-cache \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile \
-    --tag golfvert/wis2gb:2.0.11 .
+    --tag golfvert/wis2gb:2.1.0 .
